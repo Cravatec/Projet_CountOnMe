5702c74373e30c8b2c0028ea0a82d639f19d97cf
Cravatec <samuel.wrobel@gmail.com>
2022-03-22 11:13:55 +0100

    XCTest addition update

--- CountOnMeTests/SimpleCalcTests.swift
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -7,28 +7,22 @@
 //
 
 import XCTest
+
 @testable import CountOnMe
 
-class CountOnMeTests: XCTestCase {
+class CalculatorTest: XCTestCase {
 
+    var calculator: Calculator!
+
+    // init
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        calculator = Calculator()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    // test for addition
+    func testGivenCanAddOperator_WhenCanAddOperator_ThenReturnEqual() {
+        calculator.calculString = " 1 + 2 + 3 "
+        XCTAssertTrue(calculator.canAddOperator)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
